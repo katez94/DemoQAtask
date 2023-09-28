644303b4c1495be8a98256b26d7f1315d1e854e7
Ekaterina Zaikina <4815162342kate_Z>
2023-09-28 22:12:28 +0200

    compatibility bug fix for MacBook Air M1

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -2,15 +2,15 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import utils.JSUtils;
 
 import static tests.BaseTest.driver;
-import static tests.BaseTest.getActions;
 
 public class MainPage {
     private final WebElement elementsCard = driver.findElement(By.xpath("//*[contains(@class,'top-card') and .//*[contains(text(),'Elements')]]"));
 
     public void clickElementsCard() {
-        getActions().scrollToElement(elementsCard);
+        JSUtils.scrollToElement(elementsCard);
         elementsCard.click();
     }
 }
